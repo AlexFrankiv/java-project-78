5fdd16ce18a3744543c6b1fc1f24f0c0ddcab72a
franAlex <frankivs@mail.ru>
2026-03-05 18:36:07 +0500

    rewrite BaseSchema and other classes.

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,16 +3,17 @@ package hexlet.code.schemas;
 import java.util.Map;
 
 public class MapSchema extends BaseSchema<Map<?, ?>> {
+    @Override
     public MapSchema required() {
-        addPredicate(s -> s != null);
+        super.required();
         return this;
     }
     public MapSchema sizeof(int sizeMap) {
-        addPredicate(s -> s == null || (s.size() >= sizeMap));
+        addValidation("sizeof", s -> s == null || (s.size() >= sizeMap));
         return this;
     }
     public MapSchema shape(Map<String, BaseSchema<String>> schemas) {
-        addPredicate(map -> schemas.entrySet()
+        addValidation("shape", map -> schemas.entrySet()
                 .stream()
                 .allMatch(e ->
                         e.getValue().isValid(((String) map.get(e.getKey())))
--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -2,19 +2,19 @@ package hexlet.code.schemas;
 
 public class StringSchema extends BaseSchema<String> {
 
+    @Override
     public StringSchema required() {
-        addPredicate(s -> s != null && !s.isEmpty());
+        addValidation("required", s -> s != null && !s.isEmpty());
         return this;
     }
 
     public StringSchema minLength(int minSize) {
-        addPredicate(s -> s == null || s.length() >= minSize);
+        addValidation("minLength", s -> s == null || s.length() >= minSize);
         return this;
     }
 
     public StringSchema contains(String subText) {
-        addPredicate(s -> s == null || s.contains(subText));
+        addValidation("contains", s -> s == null || s.contains(subText));
         return this;
     }
-
 }
